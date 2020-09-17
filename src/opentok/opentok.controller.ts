f880cfbb66795d7d019994e3bf85cfcff48a8f33
nildiert <niljordan23@gmail.com>
2020-09-16 20:00:45 -0500

    [UPDATED] Changed the way in that opentok/room/name api return a value

--- src/opentok/opentok.controller.ts
+++ src/opentok/opentok.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, HttpStatus, } from '@nestjs/common';
 import { OpentokService } from "./opentok.service";
 import * as Opentok from 'opentok';
 
@@ -27,9 +27,14 @@ export class OpentokController {
         const session: Opentok.Session = await this.opentokService.createSession();
         const sessionId = session.sessionId;
         return {
-            apiKey: this.opentokService.getApiKey(),
-            sessionId: session.sessionId,
-            token: this.opentokService.generateToken(sessionId)
+            statusCode: HttpStatus.OK,
+            data: [{
+                apiKey: this.opentokService.getApiKey(),
+                sessionId: session.sessionId,
+                token: this.opentokService.generateToken(sessionId)
+            }
+            ]
+
         }
     }
 }
